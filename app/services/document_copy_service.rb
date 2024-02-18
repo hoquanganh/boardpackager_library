99a82e7edb4601b2c2a8630772ed2c77c17c0150
Quang Anh Ho <hoquanganh.bmt@gmail.com>
2024-02-18 19:54:10 +0700

    Save attach file when copying

--- app/services/document_copy_service.rb
+++ app/services/document_copy_service.rb
@@ -7,8 +7,15 @@ class DocumentCopyService
   def perform!
     return if @document&.private || @document.user == @user
 
-    copied_document = @document.dup
-    copied_document.user = @user
-    copied_document.save!
+    Document.transaction do
+      copied_document = @document.dup
+      copied_document.user = @user
+
+      if @document.file.attached?
+        copied_document.file.attach(@document.file.blob)
+      end
+
+      copied_document.save
+    end
   end
 end
