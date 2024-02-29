5628e19d3a43b41d67c2ada05c5bbdd3466ffb46
hoquanganh <hoquanganh.bmt@gmail.com>
2024-02-29 07:58:31 +0700

    update document copy transaction

--- app/services/document_copy_service.rb
+++ app/services/document_copy_service.rb
@@ -5,17 +5,23 @@ class DocumentCopyService
   end
 
   def perform!
-    return if @document&.private || @document.user == @user
+    return false if @document&.private || @document.user == @user
 
     Document.transaction do
-      copied_document = @document.dup
-      copied_document.user = @user
+      begin
+        copied_document = @document.dup
+        copied_document.user = @user
 
-      if @document.file.attached?
-        copied_document.file.attach(@document.file.blob)
+        if @document.file.attached?
+          copied_document.file.attach(@document.file.blob)
+        end
+
+        copied_document.save!
+        true
+      rescue StandardError => e
+        Rails.logger.error("An error occurred while performing the transaction: #{e.message}")
+        false
       end
-
-      copied_document.save
     end
   end
 end
